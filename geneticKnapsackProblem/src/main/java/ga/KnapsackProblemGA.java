a09f7f2eab1a72e17c631a8070879de98c5b1709
sebastiancepedacaceres <scepedacaceres@gmail.com>
2025-05-25 20:48:24 -0300

    Implementacion/ProblemGA

--- geneticKnapsackProblem/src/main/java/ga/KnapsackProblemGA.java
+++ geneticKnapsackProblem/src/main/java/ga/KnapsackProblemGA.java
@@ -1,7 +1,12 @@
 package ga;
 
 import org.jgap.InvalidConfigurationException;
+import org.jgap.Chromosome;
 import org.jgap.Configuration;
+import org.jgap.Gene;
+import org.jgap.Genotype;
+import org.jgap.IChromosome;
+import org.jgap.impl.BooleanGene;
 import org.jgap.impl.DefaultConfiguration;
 
 /**
@@ -16,11 +21,36 @@ public class KnapsackProblemGA {
 	 * Evolve the problem.
 	 * @param args Arguments for the main function.
 	 */
-	public static void main(String[] args) throws InvalidConfigurationException {
+public static void main(String[] args) throws InvalidConfigurationException {
     Configuration conf = new DefaultConfiguration();
+    conf.setPreservFittestIndividual(true);
+    conf.setKeepPopulationSizeConstant(false);
+
     KnapsackProblem myKnapsack = new KnapsackProblem(50);
     KnapsackFitness myFunc = new KnapsackFitness(myKnapsack);
-    //TODO: Create your own Configuration and Initial Genotype
-    //TODO: Evolve the population.
-	}
+    conf.setFitnessFunction(myFunc);
+
+    int itemCount = myKnapsack.getItemCount();
+    Gene[] genes = new Gene[itemCount];
+    for (int i = 0; i < itemCount; i++) {
+        genes[i] = new BooleanGene(conf);
+    }
+
+    Chromosome chromosome = new Chromosome(conf, genes);
+    conf.setSampleChromosome(chromosome);
+    conf.setPopulationSize(50);
+
+    Genotype population = Genotype.randomInitialGenotype(conf);
+
+    for (int i = 0; i < 100; i++) {
+        population.evolve();
+    }
+
+    IChromosome best = population.getFittestChromosome();
+    System.out.println("Mejor solución con valor: " + best.getFitnessValue());
+    for (int i = 0; i < itemCount; i++) {
+        System.out.println("Item " + i + " seleccionado: " + best.getGene(i).getAllele());
+    }
+}
+
 }
