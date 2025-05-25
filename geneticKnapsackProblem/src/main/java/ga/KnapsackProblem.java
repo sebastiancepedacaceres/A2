0133529884fbab1d079553930d8e8b4cc2d28fe1
sebastiancepedacaceres <scepedacaceres@gmail.com>
2025-05-25 20:40:19 -0300

    implementacion/KnapsackProblem

--- geneticKnapsackProblem/src/main/java/ga/KnapsackProblem.java
+++ geneticKnapsackProblem/src/main/java/ga/KnapsackProblem.java
@@ -1,15 +1,27 @@
 package ga;
 
-
-/**
-* Knapsack Problem characterization.
-*/
 public class KnapsackProblem {
-  /**
-   * KnapsackProblem constructor.
-   * @param maxCapacity maximum capacity allowed.
-   */
-  public KnapsackProblem(int maxCapacity) {
-   throw new UnsupportedOperationException("method not yet implemented");
-  }
+    private final int maxCapacity;
+    private final int[] weights = {10, 20, 30, 40, 50};
+    private final int[] values = {60, 100, 120, 240, 300};
+
+    public KnapsackProblem(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public int getWeight(int index) {
+        return weights[index];
+    }
+
+    public int getValue(int index) {
+        return values[index];
+    }
+
+    public int getItemCount() {
+        return weights.length;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
 }
